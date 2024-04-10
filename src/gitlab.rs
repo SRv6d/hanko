4478922b91247cc23ae9b5f41a3bed35a4d44b99
Marvin Vogt <m@rvinvogt.com>
2024-04-10 20:31:08 +0000

    Add test for gitlab API response deserialization

--- src/gitlab.rs
+++ src/gitlab.rs
@@ -21,6 +21,7 @@ async fn get_signing_keys(url: Url, client: Client) -> Result<Vec<SshPublicKey>>
 mod tests {
     use super::*;
     use httpmock::prelude::*;
+    use rstest::rstest;
 
     /// The API request made to get a users signing keys is correct.
     #[tokio::test]
@@ -39,5 +40,62 @@ mod tests {
         let _ = get_signing_keys(url, client).await;
 
         mock.assert();
+    }
+
+    /// Keys returned from the API are deserialized correctly.
+    #[rstest]
+    #[case("[]", vec![])]
+    #[case(
+        r#"[
+              {
+                  "id": 1121029,
+                  "title": "key-1",
+                  "created_at": "2020-08-21T19:43:06.816Z",
+                  "expires_at": null,
+                  "key": "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtQUDZWhs8k/cZcykMkaoX7ZE7DXld8TP79HyddMVTS John Doe (gitlab.com)",
+                  "usage_type": "auth_and_signing"
+              },
+              {
+                "id": 1121030,
+                "title": "key-2",
+                "created_at": "2023-07-22T23:04:29.415Z",
+                "expires_at": "2025-04-10T00:00:00.000Z",
+                "key": "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCoObGvI0R2SfxLypsqi25QOgiI1lcsAhtL7AqUeVD+4mS0CQ2Nu/C8h+RHtX6tHpd+GhfGjtDXjW598Vr2j9+w= John Doe (gitlab.com)",
+                "usage_type": "auth"
+              },
+              {
+                "id": 1121031,
+                "title": "key-3",
+                "created_at": "2023-12-04T19:32:23.794Z",
+                "expires_at": null,
+                "key": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDDTdEeUFjUX76aMptdG63itqcINvu/tnV5l9RXy/1TS25Ui2r+C2pRjG0vr9lzfz8TGncQt1yKmaZDAAe6mYGFiQlrkh9RJ/MPssRw4uS4slvMTDWhNufO1M3QGkek81lGaZq55uazCcaM5xSOhLBdrWIMROeLgKZ9YkHNqJXTt9V+xNE5ZkB/65i2tCkGdXnQsGJbYFbkuUTvYBuMW9lwmryLTeWwFLWGBP1moZI9etk3snh2hCLTV8+gvmhCTE8sAGBMcJq+TGxnfFoCtnA9Bdy7t+ZMLh1kV7oneUA9YT7qNeUFy55D287DAltB02ntT7CtuG6SBAQ4CQMcCoAX3Os4aVfdILOEC8ghrAj3uTEQuE3nYta0SmqqXcVAxmXUQCawf8n5CJ7QN5aIhCH73MKr6k5puk9dnkAcAFLRM6stvQhnpIqrI3YEbjqs1FGHfbc4+nfEWorxRrd7ur1ckEhuvmAXRKrLzYp9gYWU6TxfRqSxsXh3he0G6i+kC6k= John Doe (gitlab.com)",
+                "usage_type": "signing"
+              }
+        ]"#,
+        vec![
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtQUDZWhs8k/cZcykMkaoX7ZE7DXld8TP79HyddMVTS John Doe (gitlab.com)".parse().unwrap(),
+            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDDTdEeUFjUX76aMptdG63itqcINvu/tnV5l9RXy/1TS25Ui2r+C2pRjG0vr9lzfz8TGncQt1yKmaZDAAe6mYGFiQlrkh9RJ/MPssRw4uS4slvMTDWhNufO1M3QGkek81lGaZq55uazCcaM5xSOhLBdrWIMROeLgKZ9YkHNqJXTt9V+xNE5ZkB/65i2tCkGdXnQsGJbYFbkuUTvYBuMW9lwmryLTeWwFLWGBP1moZI9etk3snh2hCLTV8+gvmhCTE8sAGBMcJq+TGxnfFoCtnA9Bdy7t+ZMLh1kV7oneUA9YT7qNeUFy55D287DAltB02ntT7CtuG6SBAQ4CQMcCoAX3Os4aVfdILOEC8ghrAj3uTEQuE3nYta0SmqqXcVAxmXUQCawf8n5CJ7QN5aIhCH73MKr6k5puk9dnkAcAFLRM6stvQhnpIqrI3YEbjqs1FGHfbc4+nfEWorxRrd7ur1ckEhuvmAXRKrLzYp9gYWU6TxfRqSxsXh3he0G6i+kC6k= John Doe (gitlab.com)".parse().unwrap(),
+        ]
+    )]
+    #[tokio::test]
+    async fn keys_returned_by_api_deserialized_correctly(
+        #[case] body: &str,
+        #[case] expected: Vec<SshPublicKey>,
+    ) {
+        let path = "/api/v4/users/tanuki/keys";
+        let server = MockServer::start();
+        server.mock(|when, then| {
+            when.method(GET).path(path);
+            then.status(200)
+                .header("Content-Type", "application/json")
+                .body(body);
+        });
+
+        let client = Client::new();
+        let keys = get_signing_keys(server.url(path).parse().unwrap(), client)
+            .await
+            .unwrap();
+
+        assert_eq!(keys, expected);
     }
 }
