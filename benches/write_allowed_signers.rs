e6e2717c078a8e536058709f4e8918b5e0bbe54e
Marvin Vogt <m@rvinvogt.com>
2024-04-12 22:52:50 +0000

    Benchmark writing the signers file

---
+++ benches/write_allowed_signers.rs
@@ -0,0 +1,41 @@
+use chrono::{Local, TimeZone};
+use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
+use hanko::{AllowedSigner, AllowedSignersFile};
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut file = AllowedSignersFile {
+        file: tempfile::tempfile().unwrap(),
+        signers: vec![
+            AllowedSigner {
+                principal: "j.snow@wall.com".to_string(),
+                valid_after: None,
+                valid_before: None,
+                key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtQUDZWhs8k/cZcykMkaoX7ZE7DXld8TP79HyddMVTS"
+                    .parse()
+                    .unwrap(),
+            },
+            AllowedSigner {
+                principal: "ian.malcom@acme.corp".to_string(),
+                valid_after: Some(Local.with_ymd_and_hms(2024, 4, 11, 22, 00, 00).unwrap()),
+                valid_before: None,
+                key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWtK6WxXw7NVhbn6fTQ0dECF8y98fahSIsqKMh+sSo9"
+                    .parse()
+                    .unwrap(),
+            },
+            AllowedSigner {
+                principal: "cwoods@universal.exports".to_string(),
+                valid_after: None,
+                valid_before: Some(Local.with_ymd_and_hms(2030, 1, 1, 0, 0, 0).unwrap()),
+                key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHDGMF+tZQL3dcr1arPst+YP8v33Is0kAJVvyTKrxMw"
+                    .parse()
+                    .unwrap(),
+            },
+        ],
+    };
+    c.bench_function("write the signers file", |b| {
+        b.iter(|| file.write().unwrap())
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
