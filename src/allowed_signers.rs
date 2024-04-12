7166ce5ed7a6be679c85afd60d861b369be3b11c
Marvin Vogt <m@rvinvogt.com>
2024-04-12 22:06:38 +0000

    Move example signers into fixtures

--- src/allowed_signers.rs
+++ src/allowed_signers.rs
@@ -64,14 +64,13 @@ impl AllowedSignersFile {
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
-
     use super::*;
     use chrono::TimeZone as _;
-    use rstest::rstest;
+    use rstest::*;
+    use std::fs;
 
-    #[rstest]
-    #[case(
+    #[fixture]
+    fn signer_jsnow() -> AllowedSigner {
         AllowedSigner {
             principal: "j.snow@wall.com".to_string(),
             valid_after: None,
@@ -79,10 +78,11 @@ mod tests {
             key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtQUDZWhs8k/cZcykMkaoX7ZE7DXld8TP79HyddMVTS"
                 .parse()
                 .unwrap(),
-        },
-        "j.snow@wall.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtQUDZWhs8k/cZcykMkaoX7ZE7DXld8TP79HyddMVTS"
-    )]
-    #[case(
+        }
+    }
+
+    #[fixture]
+    fn signer_imalcom() -> AllowedSigner {
         AllowedSigner {
             principal: "ian.malcom@acme.corp".to_string(),
             valid_after: Some(Local.with_ymd_and_hms(2024, 4, 11, 22, 00, 00).unwrap()),
@@ -90,10 +90,11 @@ mod tests {
             key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWtK6WxXw7NVhbn6fTQ0dECF8y98fahSIsqKMh+sSo9"
                 .parse()
                 .unwrap(),
-        },
-        "ian.malcom@acme.corp valid-after=20240411220000 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWtK6WxXw7NVhbn6fTQ0dECF8y98fahSIsqKMh+sSo9"
-    )]
-    #[case(
+        }
+    }
+
+    #[fixture]
+    fn signer_cwoods() -> AllowedSigner {
         AllowedSigner {
             principal: "cwoods@universal.exports".to_string(),
             valid_after: None,
@@ -101,7 +102,20 @@ mod tests {
             key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHDGMF+tZQL3dcr1arPst+YP8v33Is0kAJVvyTKrxMw"
                 .parse()
                 .unwrap(),
-        },
+        }
+    }
+
+    #[rstest]
+    #[case(
+        signer_jsnow(),
+        "j.snow@wall.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtQUDZWhs8k/cZcykMkaoX7ZE7DXld8TP79HyddMVTS"
+    )]
+    #[case(
+        signer_imalcom(),
+        "ian.malcom@acme.corp valid-after=20240411220000 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWtK6WxXw7NVhbn6fTQ0dECF8y98fahSIsqKMh+sSo9"
+    )]
+    #[case(
+        signer_cwoods(),
         "cwoods@universal.exports valid-before=20300101000000 ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHDGMF+tZQL3dcr1arPst+YP8v33Is0kAJVvyTKrxMw"
     )]
     fn display_allowed_signer(#[case] signer: AllowedSigner, #[case] expected_display: &str) {
@@ -111,32 +125,7 @@ mod tests {
     #[test]
     fn write_allowed_signers_file() {
         let path = tempfile::NamedTempFile::new().unwrap().into_temp_path();
-        let signers = vec![
-            AllowedSigner {
-                principal: "j.snow@wall.com".to_string(),
-                valid_after: None,
-                valid_before: None,
-                key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtQUDZWhs8k/cZcykMkaoX7ZE7DXld8TP79HyddMVTS"
-                    .parse()
-                    .unwrap(),
-            },
-            AllowedSigner {
-                principal: "ian.malcom@acme.corp".to_string(),
-                valid_after: Some(Local.with_ymd_and_hms(2024, 4, 11, 22, 00, 00).unwrap()),
-                valid_before: None,
-                key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWtK6WxXw7NVhbn6fTQ0dECF8y98fahSIsqKMh+sSo9"
-                    .parse()
-                    .unwrap(),
-            },
-            AllowedSigner {
-                principal: "cwoods@universal.exports".to_string(),
-                valid_after: None,
-                valid_before: Some(Local.with_ymd_and_hms(2030, 1, 1, 0, 0, 0).unwrap()),
-                key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJHDGMF+tZQL3dcr1arPst+YP8v33Is0kAJVvyTKrxMw"
-                    .parse()
-                    .unwrap(),
-            },
-        ];
+        let signers = vec![signer_jsnow(), signer_imalcom(), signer_cwoods()];
         let mut expected_content = String::new();
         for signer in &signers {
             expected_content.push_str(&format!("{}\n", signer));
