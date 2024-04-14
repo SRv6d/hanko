e9061c294c1c3443f44a1043739c4c40ba1aaa59
Marvin Vogt <m@rvinvogt.com>
2024-04-14 21:51:44 +0000

    Enable loading configuration from TOML

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,10 @@
 use crate::{GitProvider, SshPublicKey};
+use figment::{
+    providers::{Format, Toml},
+    Figment,
+};
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 /// The main configuration.
 #[derive(Debug, Deserialize, Serialize)]
@@ -8,6 +13,13 @@ struct Config {
     organizations: Vec<Organization>,
     local: Vec<SshPublicKey>,
     sources: Vec<Source>,
+}
+
+impl Config {
+    /// Load the configuration from a TOML file at the given path.
+    fn load(path: PathBuf) -> figment::Result<Self> {
+        Figment::from(Toml::file(path)).extract()
+    }
 }
 
 #[derive(Debug, Deserialize, Serialize)]
