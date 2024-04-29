240ca2bca954c2d6e97b99a147df2b0d57942845
Marvin Vogt <m@rvinvogt.com>
2024-04-29 21:39:20 +0200

    Remove unused import

--- benches/write_allowed_signers.rs
+++ benches/write_allowed_signers.rs
@@ -1,5 +1,5 @@
 use chrono::{Local, TimeZone};
-use codspeed_criterion_compat::{black_box, criterion_group, criterion_main, Criterion};
+use codspeed_criterion_compat::{criterion_group, criterion_main, Criterion};
 use hanko::{AllowedSigner, AllowedSignersFile};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
