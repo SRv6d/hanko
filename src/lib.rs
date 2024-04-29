7266f0d051e8cfe9846df23a2e60c0e5b31ddd91
Marvin Vogt <m@rvinvogt.com>
2024-04-29 21:28:02 +0200

    Enable lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![warn(clippy::pedantic)]
+#![warn(clippy::panic)]
+#![forbid(unsafe_code)]
+
 pub use allowed_signers::{AllowedSigner, AllowedSignersFile};
 pub use config::Config;
 pub use core::*;
