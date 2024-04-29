89c6375ffccc9809cf37dc3c58c250c8348972c6
Marvin Vogt <m@rvinvogt.com>
2024-04-29 21:55:18 +0000

    Add `Provider` trait

--- src/source/main.rs
+++ src/source/main.rs
@@ -2,14 +2,14 @@ use crate::SshPublicKey;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize, PartialEq)]
-pub struct Source {
-    source_type: SourceType,
+pub struct Source<P: Provider> {
     url: String,
+    provider: P,
 }
 
-impl Source {
-    pub fn get_keys_by_username(&self, username: &str) -> Vec<SshPublicKey> {
-        todo!();
+impl<P: Provider> Source<P> {
+    pub fn new(url: String, source_type: SourceType) -> Self {
+        todo!()
     }
 }
 
@@ -19,4 +19,8 @@ impl Source {
 pub enum SourceType {
     Github,
     Gitlab,
+}
+
+trait Provider {
+    async fn get_keys_by_username(&self, username: &str) -> Vec<SshPublicKey>;
 }
