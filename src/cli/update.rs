07b8444a4b2026b2b77f373a64da534a0acbba93
Marvin Vogt <m@rvinvogt.com>
2024-05-01 21:49:23 +0000

    First `get_public_keys` implementation

--- src/cli/update.rs
+++ src/cli/update.rs
@@ -1,14 +1,18 @@
 //! The update subcommand used to get the latest allowed signers and write them to the output file.
-use crate::{AllowedSignersEntry, Configuration, SourceMap, SshPublicKey};
+use crate::{
+    config::UserConfiguration, AllowedSignersEntry, Configuration, SourceMap, SshPublicKey,
+};
 use std::collections::HashSet;
+use tokio::runtime::Runtime;
 
 pub(super) fn update(config: Configuration) {
+    let rt = Runtime::new().unwrap();
     let sources = config.get_sources();
 
     let mut allowed_signers: HashSet<AllowedSignersEntry> = HashSet::new();
     if let Some(users) = config.users {
         for user in users {
-            let public_keys = get_public_keys((), &sources);
+            let public_keys = rt.block_on(get_public_keys(&user, &sources));
             for public_key in public_keys {
                 todo!("Insert allowed signer into set.");
             }
@@ -16,6 +20,17 @@ pub(super) fn update(config: Configuration) {
     }
 }
 
-fn get_public_keys(user: (), sources: &SourceMap) -> Vec<SshPublicKey> {
-    todo!("Retrieve public keys for a user from all sources.");
+async fn get_public_keys(user: &UserConfiguration, sources: &SourceMap) -> Vec<SshPublicKey> {
+    let client = reqwest::Client::new();
+    let mut keys: Vec<SshPublicKey> = Vec::new();
+    for source_name in &user.sources {
+        let source = sources.get(source_name).unwrap();
+        keys.extend(
+            source
+                .get_keys_by_username(&user.name, &client)
+                .await
+                .unwrap(),
+        );
+    }
+    keys
 }
