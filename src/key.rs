dc8a3d8bdf150c6b04768e1a56581fb2a972c476
Marvin Vogt <m@rvinvogt.com>
2024-06-15 15:36:32 +0000

    Remove redundant `get_public_keys` fn

--- src/key.rs
+++ src/key.rs
@@ -1,8 +1,5 @@
-use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::{fmt, str::FromStr};
-
-use crate::{Source, SourceError};
 
 #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct SshPublicKey {
@@ -21,17 +18,4 @@ impl fmt::Display for SshPublicKey {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.key)
     }
-}
-
-/// Get all [`SshPublicKey`]s associated with a user from the given sources.
-pub async fn get_public_keys(
-    username: &str,
-    sources: impl IntoIterator<Item = &dyn Source>,
-    client: &Client,
-) -> Result<Vec<SshPublicKey>, SourceError> {
-    let mut keys: Vec<SshPublicKey> = Vec::new();
-    for source in sources {
-        keys.extend(source.get_keys_by_username(username, client).await?);
-    }
-    Ok(keys)
 }
