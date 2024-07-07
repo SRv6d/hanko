3679298249789e16e47709a4334ac096a8620825
Marvin Vogt <m@rvinvogt.com>
2024-07-07 19:57:12 +0000

    Add todo `get_signer_entries` fn

--- src/cli/update.rs
+++ src/cli/update.rs
@@ -1,18 +1,12 @@
 //! The update subcommand used to get the latest allowed signers and write them to the output file.
-use crate::{user::User, AllowedSignersFile};
+use crate::{user::User, AllowedSignersEntry, AllowedSignersFile};
 use anyhow::{Context, Result};
-use std::path::Path;
+use std::{path::Path, rc::Rc};
 
 #[tokio::main]
 #[tracing::instrument]
 pub(super) async fn update(path: &Path, users: &Vec<User>) -> Result<()> {
-    let client = reqwest::Client::new();
-
-    let mut entries = Vec::new();
-    for user in users {
-        entries.extend(user.get_allowed_signers(&client).await?);
-    }
-
+    let entries = get_signer_entries(users).await?;
     let file = AllowedSignersFile::with_signers(path.into(), entries);
     file.write().context(format!(
         "Failed to write allowed signers file to {}",
@@ -20,4 +14,9 @@ pub(super) async fn update(path: &Path, users: &Vec<User>) -> Result<()> {
     ))?;
 
     Ok(())
+}
+
+async fn get_signer_entries(users: &[User<'_>]) -> Result<Vec<AllowedSignersEntry>> {
+    let client = Rc::new(reqwest::Client::new());
+    todo!()
 }
