5749ca99c34d022e7a3f8935d175425cc525bd0a
Marvin Vogt <m@rvinvogt.com>
2024-10-13 20:01:14 +0000

    Rename `allowed_singers` runtime and configuration option to just `file`

--- tests/update_cmd.rs
+++ tests/update_cmd.rs
@@ -140,7 +140,7 @@ fn update_writes_expected_allowed_signers(
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     cmd.arg("--config")
         .arg(config.path())
-        .arg("--allowed-signers")
+        .arg("--file")
         .arg(allowed_signers.path())
         .arg("update")
         .assert()
