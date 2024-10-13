a891b28a1a456e8f2a9887f2700cc65abf8db918
Marvin Vogt <m@rvinvogt.com>
2024-10-13 19:15:02 +0000

    Reimplement get_entries

--- src/allowed_signers/signer.rs
+++ src/allowed_signers/signer.rs
@@ -1,9 +1,10 @@
 use std::sync::Arc;
 
 use tokio::task::JoinSet;
+use tracing::debug;
 
-use super::file::Entry;
-use crate::source::Source;
+use super::{file::Entry, ssh::PublicKey};
+use crate::{source::Source, SourceError};
 
 /// An allowed signer.
 #[derive(Debug)]
@@ -14,9 +15,32 @@ pub struct Signer {
 }
 
 impl Signer {
+    /// Get the signers public keys from all of it's sources.
+    #[tracing::instrument(skip_all, fields(username=self.name), level = "debug")]
+    async fn get_keys(&self) -> Result<Vec<PublicKey>, SourceError> {
+        debug!("Getting keys from users configured sources");
+        // TODO: Join futures
+        let mut keys = Vec::new();
+        for source in &self.sources {
+            // TODO: Handle some error cases gracefully, e.g if the user has no keys, while returning others.
+            keys.extend(source.get_keys_by_username(&self.name).await?);
+        }
+        Ok(keys)
+    }
+
     /// Get the allowed signers file entries corresponding to this signer.
-    pub(super) async fn get_entries(&self) -> Result<Vec<Entry>, ()> {
-        todo!()
+    pub(super) async fn get_entries(&self) -> Result<Vec<Entry>, SourceError> {
+        let keys = self.get_keys().await?;
+
+        Ok(keys
+            .into_iter()
+            .map(|key| Entry {
+                principals: self.principals.clone(),
+                valid_after: None,
+                valid_before: None,
+                key,
+            })
+            .collect())
     }
 }
 
