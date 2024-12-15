fb80c27da4b90567aa4cb081b542cb6f6779e753
Marvin Vogt <m@rvinvogt.com>
2024-12-15 00:38:52 +0000

    Add xtask to generate manpages

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,7 +1,6 @@
 use clap::{self, CommandFactory, ValueEnum};
-use clap_complete::{generate_to, Shell};
 use hanko::cli::Cli;
-use std::{env, error::Error};
+use std::{env, error::Error, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let task = env::args().nth(1);
@@ -9,6 +8,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         Some("completions") => {
             let output_dir = env::args().nth(2).expect("output directory not specified");
             create_completions(output_dir.into())?
+        }
+        Some("manpages") => {
+            let output_dir =
+                PathBuf::from(env::args().nth(2).expect("output directory not specified"));
+            create_manpages(output_dir)?
         }
         _ => print_help(),
     }
@@ -18,9 +22,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 /// Create command completions for all shells supported by clap_complete.
 fn create_completions(dir: clap::builder::OsStr) -> Result<(), Box<dyn Error>> {
     let mut cmd = Cli::command();
-    for &shell in Shell::value_variants() {
-        generate_to(shell, &mut cmd, "hanko", &dir)?;
+    for &shell in clap_complete::Shell::value_variants() {
+        clap_complete::generate_to(shell, &mut cmd, "hanko", &dir)?;
     }
+
+    Ok(())
+}
+
+/// Create manpages for all commands and subcommands.
+fn create_manpages(dir: PathBuf) -> Result<(), Box<dyn Error>> {
+    let cmd = Cli::command();
+    clap_mangen::generate_to(cmd, dir)?;
 
     Ok(())
 }
@@ -29,6 +41,7 @@ fn print_help() {
     eprintln!(
         "Tasks:
 
+manpages            generate manpages
 completions         generate shell completions
 "
     )
