d6c3756a1019d845704cc92ad7691d22df3cef73
Marvin Vogt <m@rvinvogt.com>
2026-01-01 16:36:05 +0000

    Fix deprecated usage of `assert_cmd::Command::cargo_bin`

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,12 +1,12 @@
 //! General top level CLI tests.
-use assert_cmd::Command;
+use assert_cmd::cargo::cargo_bin_cmd;
 use predicates::prelude::*;
 
 #[test]
 fn version_contains_version() {
     let version = format!("hanko {}", env!("CARGO_PKG_VERSION"));
 
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let mut cmd = cargo_bin_cmd!();
     cmd.arg("--version");
 
     cmd.assert().success();
