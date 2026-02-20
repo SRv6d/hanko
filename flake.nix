f8032aca04530157342de8f045951c2391578ba9
Marvin Vogt <m@rvinvogt.com>
2026-02-20 22:40:59 +0000

    Add flake for packaging

--- flake.nix
+++ flake.nix
@@ -18,6 +18,12 @@
         gitRev = self.shortRev or self.dirtyShortRev or null;
 
         build = import ./nix/build.nix { inherit pkgs crane rustToolchain gitRev; };
+
+        packaging = import ./nix/package.nix {
+          inherit pkgs;
+          inherit (build) packages;
+        };
+
       in
       {
         packages = {
@@ -25,7 +31,16 @@
           default = build.packages.${build.nativeTarget};
           rust-toolchain = rustToolchain;
           rust-analyzer  = pkgs.rust-analyzer;
-        } // build.packages;
+        } // build.packages
+          // pkgs.lib.mapAttrs'
+            (target: archive: pkgs.lib.nameValuePair "archive-${target}" archive)
+            packaging.archives
+          // pkgs.lib.mapAttrs'
+            (target: deb: pkgs.lib.nameValuePair "deb-${target}" deb)
+            packaging.debs
+          // pkgs.lib.mapAttrs'
+            (target: img: pkgs.lib.nameValuePair "container-${target}" img)
+            packaging.containerImages;
 
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
