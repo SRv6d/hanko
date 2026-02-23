b128c99a4f8f23e91f9f0500efcb907c9af93433
Marvin Vogt <m@rvinvogt.com>
2026-02-23 21:50:20 +0000

    Return outcome form `allowed_signers::update` and report on it

--- tests/update_cmd.rs
+++ tests/update_cmd.rs
@@ -176,7 +176,6 @@ fn update_warns_when_no_entries_and_file_exists(mock_github_server: MockServer) 
     allowed_signers.write_all(b"cwoods@universal.exports ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGtQUDZWhs8k/cZcykMkaoX7ZE7DXld8TP79HyddMVTS John Doe (gitlab.com)").unwrap();
 
     cargo_bin_cmd!()
-        .arg("-v")
         .arg("--config")
         .arg(config.path())
         .arg("--file")
@@ -184,7 +183,7 @@ fn update_warns_when_no_entries_and_file_exists(mock_github_server: MockServer) 
         .arg("update")
         .assert()
         .success()
-        .stdout(predicate::str::contains(
+        .stderr(predicate::str::contains(
             "No allowed signer entries collected",
         ));
 }
