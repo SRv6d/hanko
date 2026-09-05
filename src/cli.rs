1f1c0028b8e0bd1e73849728644ecbe06a813475
Marvin Vogt <m@rvinvogt.com>
2026-09-05 15:48:54 +0200

    Fix compilation issue

--- src/cli.rs
+++ src/cli.rs
@@ -112,7 +112,7 @@ fn git_allowed_signers() -> Resettable<OsStr> {
             ..Default::default()
         })
     {
-        return Resettable::Value(OsStr::from(interpolated.into_owned().into_os_string()));
+        return Resettable::Value(interpolated.into_os_string().into());
     }
 
     Resettable::Reset
